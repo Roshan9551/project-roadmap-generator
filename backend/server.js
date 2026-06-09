af8f98a2f65e7d313799fd571aaeae6e1f0a2d42
Roshan9551 <sunarroshan9956@gmail.com>
2026-06-09 15:44:05 +0545

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Pathshala backend is running" })
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Mongodb connected successfully");
+
+    app.listen(PORT, ()=>{
+        console.log(`Server running on port: ${PORT}`)
+    });
+}).catch((err) => {
+    console.log("MongoDB connection error: ", err)
+});
